43f91afe2f19c25178e6960b46e507e5943e3de5
stepforwardtech <jrstrayhorn@gmail.com>
2017-09-26 06:57:30 -0400

    Working on Async in Redux; fixing course reducer, dispatch action on page load and create courses list component

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,6 @@
 import * as types from './actionTypes';
+// when ready to hit real api just change this import to point
+// to live api
 import courseApi from '../api/mockCourseApi';
 
 export function loadCoursesSuccess(courses) {
@@ -8,12 +10,17 @@ export function loadCoursesSuccess(courses) {
     };
 }
 
+/* THUNKS */
 export function loadCourses() {
+    /* wrapper for thunk - a function that returns a function
+       to delay the run of dispatch until after courseApi promise
+       is finished */
     return function(dispatch) {
+        /* body of thunk */
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
             throw(error);
         });
-    }
+    };
 }
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,22 +2,20 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor() {
         super();
     }
 
-    courseRow(course, index) {
-        return <div key={index}>{course.title}</div>;
-    }
-
     render() {
+        const {courses} = this.props;
+
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                
+                <CourseList courses={courses}/>
             </div>
         );
     }
